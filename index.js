4d044fe716e4a71d18bb571e7e5ea9ff1a525787
Yan Emanuel <yanemanuel20@outlook.com>
2022-10-16 11:13:21 -0300

    projeto de cursoss

---
+++ index.js
@@ -0,0 +1,15 @@
+function aparecer(event){
+    var aparece = document.querySelector('.desaparecer');
+    aparece.setAttribute('style', 'display: block;', 'width: 400px')
+    
+   
+    
+     
+}
+function sumir(){
+    var suma = document.querySelector('.desaparecer');
+    suma.setAttribute('style', 'display: none;');
+  
+    
+    
+}
